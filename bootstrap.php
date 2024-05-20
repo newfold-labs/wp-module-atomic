99a15200e622811ef147d2579828856e9c93f605
Evan Mullins <evanrm@gmail.com>
2024-05-20 17:05:23 -0400

    remove disable feature methods and features module as a dependency

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,6 @@
  */
 
 use function NewfoldLabs\WP\Context\getContext;
-use function NewfoldLabs\WP\Module\Features\disable as disableFeature;
 
 if ( function_exists( 'add_action' ) ) {
 
@@ -14,17 +13,15 @@ if ( function_exists( 'add_action' ) ) {
             if ( 'atomic' === getContext( 'platform' ) ) {
 
                 // Disable performance
-                disableFeature( 'performance' );
                 add_filter( 'newfold/features/filter/canToggle:performance', '__return_false' );
                 add_filter( 'newfold/features/filter/isEnabled:performance', '__return_false' );
                 
                 // Disable staging
-                disableFeature( 'staging' );
                 add_filter( 'newfold/features/filter/canToggle:staging', '__return_false' );
                 add_filter( 'newfold/features/filter/isEnabled:staging', '__return_false' );
 
             }
-        }.
+        },
         11
     );
 }
